e9411a462698099c5598c39a9e4623f4ef20f4b0
Konstantin Nazarov <mail@knazarov.com>
2021-01-09 16:32:22 +0300

    Simplify the repository, get rid of unnecessary stuff

--- setup.py
+++ setup.py
@@ -1,7 +1,21 @@
+import os
+
 from setuptools import setup
 
-with open("VERSION") as version_file:
-    version = version_file.read().strip()
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, "README.md")) as f:
+    long_description = f.read().decode('utf8')
 
-# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
-setup(name="gevent-queue", version=version, install_requires=["redis>=3.0.0"])
+setup(
+    name="gevent-queue",
+    url="https://github.com/knazarov/gevent-queue",
+    description="A persistent multi-producer multi-consumer gevent queue",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version="0.1.3",
+    install_requires=["redis>=3.0.0"],
+    license="BSD-3-Clause",
+    author="Konstantin Nazarov",
+    author_email="mail@knazarov.com",
+    py_modules=["gevent_queue"],
+)
